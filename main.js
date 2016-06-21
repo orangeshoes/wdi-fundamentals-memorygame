e0433f79d6807646c3cf778c0771a974bc34c7ec
orangeshoes <yuzeng9@hotmail.com>
2016-06-21 23:32:02 +0800

    first java addition

--- main.js
+++ main.js
@@ -4,3 +4,21 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+
+
+if (cardOne === cardTwo) {
+	alert('Yay.');
+} else if (cardOne === cardThree) {
+	alert('No 1');
+} else if (cardOne === cardFour ) {
+	alert('No 2');
+} else if (cardThree === cardFour) {
+	alert('Yay 2.');
+} else if (cardTwo === cardFour ) {
+	alert('No 3'); 
+} else if (cardThree === cardTwo ) {
+	alert('No 4'); 
+} else { 
+	alert('nullalert');
+}
